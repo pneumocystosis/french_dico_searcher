1990ab19a8120b0f9b556111c7085ec9c883b9b6
pneumocystosis <69307109+pneumocystosis@users.noreply.github.com>
2020-10-05 15:01:37 +0200

    Add files via upload

---
+++ travail bruh 13.py
@@ -0,0 +1,25 @@
+fichier = open(r"S:\c602\donnees\1NSI\dico.txt")
+long = int(input("Quelle longueur de mot? "))
+lettres = list(input("Quelles sont les lettres nécessaires?(séparées par un espace): ").split())
+listemots = []
+listLignes = list(fichier.readlines())
+
+def longu(long, ligne):
+    if int(len(ligne)) == long+1:
+        return True
+
+def chars(lettres):
+    status = True
+    for j in range (len(lettres)):
+            if ligne.count(lettres[j]) == 0:
+                status = False
+    if status:
+        return True
+            
+for ligne in listLignes:
+    if longu(long, ligne) and chars(lettres):
+        listemots.append(ligne)
+    
+print("".join(listemots))
+
+
